c7f0cf24212b631851d6bbe9c76b4967de6f48e7
Nuria Aires <nuria.airesc@gmail.com>
2021-11-25 13:57:18 +0000

    some changes, now it is okay

--- Edamam_api.py
+++ Edamam_api.py
@@ -3,30 +3,28 @@ import collections
 from keys import get_edamam_key
 
 
-dish = "ceviche"
-api_id = "4bc274b4"
 api_key = get_edamam_key()
 
-def get_recipe(dish, api_id, api_key):
+def get_recipe(dish):
 
     #call API
-    url = f"https://api.edamam.com/api/recipes/v2?type=public&q={dish}&app_id={api_id}&app_key={api_key}"
+    url = f"https://api.edamam.com/api/recipes/v2?type=public&q={dish}&app_id=4bc274b4&app_key={api_key}"
 
-    response = requests.request("GET",url)
-    recipe = response.json()["hits"][0]['recipe']['ingredients']
+    response = requests.request("GET", url)
+    recipe = response.json()["hits"][0]["recipe"]["ingredients"]
 
     return recipe
 
-def get_ingredients(dish, api_id, api_key):
+def get_ingredients(dish):
 
     #get ingredients from the call
-    ingredients = get_recipe(dish, api_id, api_key)
+    ingredients = get_recipe(dish)
 
     new_dict = collections.defaultdict(list)
 
     for i in ingredients:
-        new_dict["ingredient"].append(i['food'])
-        new_dict["value"].append(i['quantity'])
-        new_dict["metric"].append(i['measure'])
+        new_dict["ingredient"].append(i["food"].lower())
+        new_dict["value"].append(round(i["quantity"], 2))
+        new_dict["metric"].append(i["measure"])
 
     return new_dict
