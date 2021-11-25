7009ecb3f23045cab80c434efca68a6fca12a80b
Nuria Aires <nuria.airesc@gmail.com>
2021-11-25 14:33:44 +0000

    added servings of recipes

--- Edamam_api.py
+++ Edamam_api.py
@@ -10,21 +10,23 @@ def get_recipe(dish):
     #call API
     url = f"https://api.edamam.com/api/recipes/v2?type=public&q={dish}&app_id=4bc274b4&app_key={api_key}"
 
-    response = requests.request("GET", url)
-    recipe = response.json()["hits"][0]["recipe"]["ingredients"]
+    response = requests.request("GET",url)
+    recipe = response.json()["hits"][0]['recipe']
 
     return recipe
 
 def get_ingredients(dish):
 
     #get ingredients from the call
-    ingredients = get_recipe(dish)
+    recipe = get_recipe(dish)
+    servings = recipe['yield']
+    ingredients = recipe['ingredients']
 
     new_dict = collections.defaultdict(list)
 
     for i in ingredients:
-        new_dict["ingredient"].append(i["food"].lower())
-        new_dict["value"].append(round(i["quantity"], 2))
-        new_dict["metric"].append(i["measure"])
+        new_dict["ingredient"].append(i['food'].lower())
+        new_dict["value"].append(round((i['quantity'] / servings), 2))
+        new_dict["metric"].append(i['measure'])
 
     return new_dict
