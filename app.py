366cf8201da6672d3e92d5e4ad91e8256e07e81f
Mateo Beacco <mateo.beacco-balague@hec.edu>
2021-11-29 18:05:33 +0000

    Pushed everything to the front

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import PIL
 from PIL import Image
 import numpy as np 
 import streamlit as st 
-import requests
 import json
 from Recipe_API.utils import *
 from Recipe_API.keys import *
@@ -11,6 +10,23 @@ from Edamam_api import *
 import nltk
 from nltk.stem import WordNetLemmatizer 
 import base64
+from tensorflow import keras,image
+from tensorflow.image import resize
+
+@st.cache(allow_output_mutation=True)
+def get_model():
+    model = keras.models.load_model('models/100_84_DN121_TL3_GAP.h5')
+    return model
+
+def load_classes(classes_path,index):
+    classes=pd.read_csv(classes_path)
+    return classes.iloc[index,0]
+
+
+model_path="models/100_84_DN121_TL3_GAP.h5"
+# model_path='/home/mateo/code/Matbeac/fritz/models/10_86_7_DN121_AUG_TV_ES5_RLR1_TL2_ES5_RLR1_TL3.h5'
+classes_path= 'models/100_84_DN121_TL3_GAP.csv'
+model=get_model()
 
 nltk.download('wordnet')
 
@@ -26,7 +42,6 @@ url = "https://fritz-carbon-calc-y3qsfujzsq-uc.a.run.app/predict"
 
 # CACHE :Loading the model
 response = requests.get(url).json()
-
 # Title
 st.markdown("""
     # 🥙 FRITZ
@@ -40,13 +55,20 @@ uploadFile = st.file_uploader(label="🥘Upload image", type=['jpg', 'png'])
 
 # Checking the Format of the page
 if uploadFile is not None:
-    
-    # To send to API
     img = load_resize_image(uploadFile)
-    image_coded = img.astype('uint8')
-    height, width, channel = img.shape 
-    img_reshape = image_coded.reshape(height*width*channel)
-    img_enc = base64.b64encode(img_reshape)
+    response_reshape = resize(img,[224, 224])
+    
+    # Getting the output    
+    probabilities=model.predict(np.array([response_reshape/255]))
+    index=np.argmax(probabilities)
+    recipe = load_classes(classes_path,index)
+    
+    # # To send to API
+    # img = load_resize_image(uploadFile)
+    # image_coded = img.astype('uint8')
+    # height, width, channel = img.shape 
+    # img_reshape = image_coded.reshape(height*width*channel)
+    # img_enc = base64.b64encode(img_reshape)
     
     #Print image
     st.image(img)
@@ -60,16 +82,15 @@ if uploadFile is not None:
     # X=X.tolist()
     # X_json=json.dumps(X)
     
-    # Call the POST
-    data={"image_reshape":img_enc.decode('utf8').replace("'", '"'),
-                     "height": height,
-                     "width": width,
-                     "color": channel}
-    headers = {'Content-type': 'application/json'}
-    response = requests.post(url,json.dumps(data),headers=headers).json()
-    st.write(response)
-    response = response.replace("_", " ")
-    st.write(f"FRITZ thinks the recipe is a **{response}**")
+    # # Call the POST
+    # data={"image_reshape":img_enc.decode('utf8').replace("'", '"'),
+    #                  "height": height,
+    #                  "width": width,
+    #                  "color": channel}
+    # headers = {'Content-type': 'application/json'}
+    # response = requests.post(url,json.dumps(data),headers=headers).json()
+    recipe = recipe.replace("_", " ")
+    st.write(f"FRITZ thinks the recipe is a **{recipe}**")
     
     st.write("🦑FRITZ is finding the ingredients")
     # st.write(getingredients(response))
