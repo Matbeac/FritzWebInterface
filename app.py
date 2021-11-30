91ae1b34514def225dabaeecd8cf7fdb59c5fdf9
Mateo Beacco <mateo.beacco-balague@hec.edu>
2021-11-30 12:21:50 +0000

    Everything done

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ from tensorflow import keras,image
 from tensorflow.image import resize
 nltk.download('wordnet')
 
-
+#--------------------------------------------
+# I. GETTING MODELS AND CLASSES
+#--------------------------------------------
 @st.cache(allow_output_mutation=True)
 def get_model():
     model = keras.models.load_model('models/100_84_DN121_TL3_GAP.h5')
@@ -24,13 +26,14 @@ def load_classes(classes_path,index):
     classes=pd.read_csv(classes_path)
     return classes.iloc[index,0]
 
-
+# Loading the model when the user opens the website
 model_path="models/100_84_DN121_TL3_GAP.h5"
 classes_path= 'models/100_84_DN121_TL3_GAP.csv'
 model=get_model()
 
-
-# Function to Read and Convert Images
+#--------------------------------------------
+# II. Read and Convert Images
+#--------------------------------------------
 def load_resize_image(img):
     im = Image.open(img)
     # image = im.resize((224,224)) 
@@ -38,75 +41,74 @@ def load_resize_image(img):
     return image
 
 
-# CACHE :Loading the model
-# Title
+#--------------------------------------------
+# III. FRONT END TITLE
+#--------------------------------------------
 st.markdown("""
     # 🥙 FRITZ
 
     ## The first Meal Carbon Footprint Calculator powered by Deep Learning
 """)
+# Anecdote 
 st.markdown("""Did you know that you **save more water** by **not eating** a steak than you would by **not showering** for **one month** ?""")
 
-# Uploading the Image to the Page
+#--------------------------------------------
+# IV. IMAGE UPLOAD
+#--------------------------------------------
 uploadFile = st.file_uploader(label="🥘Upload image", type=['JPEG', 'PNG','JPG'])
 
-# Checking the Format of the page
 if uploadFile is not None:
+    
     img = load_resize_image(uploadFile)
     response_reshape = resize(img,[224, 224])
     
+    #--------------------------------------------
+    # V. MODEL PREDICTION 
+    #--------------------------------------------
     # Getting the output    
     probabilities=model.predict(np.array([response_reshape/255]))
-    #[0.45,0.56,0.44]
+    
+    ## output = 100 probabilities for each class = [0.45,0.56,0.44 etc.]
     index=np.argmax(probabilities)
+    
+    ## Recipe result = the most probable output
     recipe = load_classes(classes_path,index)
     
-    # # To send to API
-    # img = load_resize_image(uploadFile)
-    # image_coded = img.astype('uint8')
-    # height, width, channel = img.shape 
-    # img_reshape = image_coded.reshape(height*width*channel)
-    # img_enc = base64.b64encode(img_reshape)
-    
+    #--------------------------------------------
+    # VI. FRONT END DESIGN OF THE LOADING
+    #--------------------------------------------
     #Print image
     st.image(img)
-
-    # st.write(img)
     st.write("📸 Image Uploaded Successfully !")
     st.write("🧠Wait a minute, FRITZ is identifying the recipe")
 
-    # Reshape the image
-    # X = img.reshape(img.shape[0]*img.shape[1]*img.shape[2])
-    # X=X.tolist()
-    # X_json=json.dumps(X)
     
-    # # Call the POST
-    # data={"image_reshape":img_enc.decode('utf8').replace("'", '"'),
-    #                  "height": height,
-    #                  "width": width,
-    #                  "color": channel}
-    # headers = {'Content-type': 'application/json'}
-    # response = requests.post(url,json.dumps(data),headers=headers).json()
     recipe = recipe.replace("_", " ")
     st.write(f"FRITZ thinks the recipe is a **{recipe}**")
     
     st.write("🦑FRITZ is finding the ingredients")
-    # st.write(getingredients(response))
 
-
-    # Try packaging
+    #--------------------------------------------
+    # VII. DATA ENGINEERING
+    #--------------------------------------------
+    ## Filling the NaNs
     output_dict=getingredients(recipe)
     output_df=fill_empties(output_dict)
     st.write(fill_empties(output_dict))
+    
+    ## Matching the ingredients with final_ingredients_emissions.csv
     final_df, missing_ingredients=match_ingredients(output_df)
     st.write(final_df)
     st.write(missing_ingredients)
     # st.write(final_df, missing_ingredients)
+    
+    
+    ## Computing the final emissions
     st.write(convert(final_df))
     final_result=round(convert(final_df)["calculated gCO2e"].sum())
     st.write(f"1 portion of this {recipe} emits {final_result} grams of C02")
     
-    # Equivalents 
+    ## Equivalents 
     miles_per_Kg = round(final_result*0.001*(296/116),2)
     heating_per_Kg = round(final_result*0.001*(29/116),2)
     showers_per_Kg = round((final_result*0.001*(18/116)),2)
@@ -118,7 +120,7 @@ if uploadFile is not None:
     col3.metric("Showers", showers_per_Kg, "+4.87%")
     col4.metric("Netflix", stream_hrs_kg, "+4.87%")
 
-    
+# If the image does not work
 else:
     st.write("Make sure you image is in JPEG/JPG/PNG Format.")
 
