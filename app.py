ad24b5aba3cdf01f51a8138bda1941547f68e78d
Mateo Beacco <mateo.beacco-balague@hec.edu>
2021-11-30 12:04:30 +0000

    Everything done

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ if uploadFile is not None:
     st.write(missing_ingredients)
     # st.write(final_df, missing_ingredients)
     st.write(convert(final_df))
-    final_result=convert(final_df)["calculated gCO2e"].sum()
+    final_result=round(convert(final_df)["calculated gCO2e"].sum())
     st.write(f"1 portion of this {recipe} emits {final_result} grams of C02")
     
     # Equivalents 
@@ -112,11 +112,11 @@ if uploadFile is not None:
     showers_per_Kg = round((final_result*0.001*(18/116)),2)
     stream_hrs_kg= round(final_result*0.001*(1/float(55/1000)),2)
     # Columns
-    # col1, col2, col3,col4 = st.columns(4)
-    # col1.metric("🚗Miles driven", miles_per_Kg, "-$1.25")
-    # col2.metric("Heating", heating_per_Kg, "0.46%")
-    # col3.metric("Showers", showers_per_Kg, "+4.87%")
-    # col4.metric("Netflix", stream_hrs_kg, "+4.87%")
+    col1, col2, col3,col4 = st.columns(4)
+    col1.metric("🚗Miles driven", miles_per_Kg, "-$1.25")
+    col2.metric("Heating", heating_per_Kg, "0.46%")
+    col3.metric("Showers", showers_per_Kg, "+4.87%")
+    col4.metric("Netflix", stream_hrs_kg, "+4.87%")
 
     
 else:
